9b70a3f619b1792a5145175bf1faccc351a870ae
Adonis <993825594@qq.com>
2016-03-09 21:48:58 +0800

    handle

--- app/src/main/java/cn/adonis/photogallery/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/cn/adonis/photogallery/fragment/PhotoGalleryFragment.java
@@ -1,21 +1,28 @@
 package cn.adonis.photogallery.fragment;
 
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import cn.adonis.photogallery.FlickrFetchr;
+import cn.adonis.photogallery.GalleryItem;
 import cn.adonis.photogallery.R;
+import cn.adonis.photogallery.ThumbnailDownloader;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +30,10 @@ import cn.adonis.photogallery.R;
 public class PhotoGalleryFragment extends Fragment {
 
     private GridView mGridView;
+    private ArrayList<GalleryItem> mItems;
+    private ThumbnailDownloader<ImageView> mThumbnailDownloader;
+    private static final String TAG="PhotoGalleryFragment";
+
     public PhotoGalleryFragment() {
         // Required empty public constructor
     }
@@ -32,6 +43,19 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         new FetchItemsTask().execute();
+
+        mThumbnailDownloader=new ThumbnailDownloader<ImageView>(new Handler());
+        mThumbnailDownloader.setListener(new ThumbnailDownloader.Listener<ImageView>(){
+            @Override
+            public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail) {
+                if(isVisible()){
+                    imageView.setImageBitmap(thumbnail);
+                }
+            }
+        });
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG,"Background thread started");
     }
 
     @Override
@@ -40,12 +64,13 @@ public class PhotoGalleryFragment extends Fragment {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView=(GridView)v.findViewById(R.id.gridView);
+        setupAdapter();
         return v;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void>{
+    private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>>{
         @Override
-        protected Void doInBackground(Void... params) {
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
 //            try{
 //                String result = new FlickrFetchr().getUrl("http://www.google.com");
 //                if(result==null)
@@ -55,9 +80,53 @@ public class PhotoGalleryFragment extends Fragment {
 //                e.printStackTrace();
 //                Log.e("fail","failed to fetch");
 //            }
-            new FlickrFetchr().fetchItems();
-            return null;
+            return new FlickrFetchr().fetchItems();
+//            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems=items;
+            setupAdapter();
         }
     }
 
+    private void setupAdapter(){
+        if(getActivity()==null||mGridView==null)
+            return;
+        if(mItems!=null){
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
+        }else {
+            mGridView.setAdapter(null);
+        }
+    }
+
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem>{
+        public GalleryItemAdapter(ArrayList<GalleryItem> items){
+            super(getActivity(),0,items);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView==null){
+                convertView=getActivity().getLayoutInflater().inflate(R.layout.gallery_item,parent,false);
+            }
+            ImageView imageView=(ImageView)convertView.findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.mipmap.brian_up_close);
+            GalleryItem item=getItem(position);
+            mThumbnailDownloader.queueThumbnail(imageView,item.getUrl());
+            return convertView;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
+    }
 }
